4173b6124137d616d19c6610ebcbaf4ea155d3cd
Francisco <francisco.anpereira@gmail.com>
2019-07-11 23:00:27 -0300

    created simple crud using angular and jsonserver

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { CidadeService } from './cidade.service';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [CidadeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
